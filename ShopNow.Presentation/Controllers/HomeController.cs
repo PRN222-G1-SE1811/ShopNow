c268cf1e60644c5804277b629ee6c8d71c2c19aa
minhkhai10122003 <khainmhe171207@fpt.edu.vn>
2025-03-03 14:34:50 +0700

    tét

--- ShopNow.Presentation/Controllers/HomeController.cs
+++ ShopNow.Presentation/Controllers/HomeController.cs
@@ -6,7 +6,7 @@ using System.Diagnostics;
 
 namespace ShopNow.Presentation.Controllers
 {
-	// test NghiNV
+	// test NghiNVkkkk 
 	public class HomeController : Controller
 	{
 		private readonly ILogger<HomeController> _logger;
