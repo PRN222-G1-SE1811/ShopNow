b16e2813837f1940966eef4a7bb767433712186c
NghiNV <nghinv03@gmail.com>
2025-03-23 00:22:26 +0700

    Reset dashboard

---
+++ ShopNow.Presentation/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShopNow.Presentation.Controllers
+{
+	public class ProductController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Manage()
+		{
+			ViewData["active"] = "product";
+			return View();
+		}
+	}
+}
