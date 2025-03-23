0a732bf00bff2a326b6afdad3757c6ae7e887748
NghiNV <nghinv03@gmail.com>
2025-03-23 17:14:58 +0700

    OrderItemAttributes table

---
+++ ShowNow.Domain/Entities/OrderItemAttributes.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShowNow.Domain.Entities
+{
+	public class OrderItemAttributes : BaseEntity<Guid>
+	{
+		public Guid OrderItemId { get; set; }
+		public Guid AttributeId { get; set; }
+
+		[ForeignKey("OrderItemId")]
+		public OrderItem OrderItem { get; set; }
+
+		[ForeignKey("AttributeId")]
+		public Attribute Attribute { get; set; }
+	}
+}
