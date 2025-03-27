e4cc439dfc2b7dd46b7b8303e5c1b42a86174faa
tubahe171912 <buianhtu293@gmail.com>
2025-03-27 09:38:55 +0700

    commit before role and css

--- ShopNow.Application/ApplicationService.cs
+++ ShopNow.Application/ApplicationService.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShopNow.Application.DTOs.Mail;
 using ShopNow.Application.Services.Implements;
 using ShopNow.Application.Services.Interfaces;
 
@@ -6,12 +9,25 @@ namespace ShopNow.Application
 {
 	public static class ApplicationService
 	{
-		public static void AddApplicationService(this IServiceCollection services)
+		public static void AddApplicationService(this IServiceCollection services, IConfiguration configuration)
 		{
 			services.AddScoped<IStorageService, StorageService>();
 			services.AddAutoMapper(typeof(ApplicationService));
 			#region add category service
 			services.AddScoped<ICategoryService, CategoryService>();
+            #endregion
+
+            #region send mail
+            //Dang ki mail
+            services.AddOptions();
+            var mailsetting = configuration.GetSection("MailSettings");
+            services.Configure<MailSettings>(mailsetting);
+            services.AddSingleton<IEmailSender, SendMailService>();
+            #endregion
+
+            #region send mail
+            //IdentityErrorServices
+            services.AddSingleton<IdentityErrorDescriber, AppIdentityErrorDescriber>();
 			#endregion
 		}
 	}
